f9d2c782f47d533cb8f53eef04101107d5e6ba76
Ben Weissmann <benweissmann@gmail.com>
2018-02-01 15:20:43 -0500

    Add workaround

--- README.md
+++ README.md
@@ -4,11 +4,15 @@
 2) Initialize the replica set: `cat initializeReplset.mongojs | mongo localhost:27077`
 3) Start Meteor: `./runApp.sh`
 
-You can observe the bug by watching the output of `docker-compose` and seeing
-that we're issuing a query after each update (which you can trigger with the
-"Click Me" button in the app; I've edited the default app to use Mongo rather
-than a ReactiveVar).
+You can observe the bug by watching the output of `docker-compose` (Mongo has
+been configured to log every query) and seeing that we're issuing a query after
+each update (which you can trigger with the "Click Me" button in the app; I've
+edited the default app to use Mongo rather than a ReactiveVar).
 
 You can also observe the bug by attaching a node inspector. Setting a breakpoint
 on `local_collection.js:1139` (on ``throw MinimongoError(`Invalid modifier specified ${operator}`);``)
 will show an error for every oplog message.
+
+To test the workaround, run the app as `USE_WORKAROUND=true ./runApp.sh`. Note
+that in the Mongo logs, there's no find after the update to fetch the new
+version of the Counter document.
--- server/main.js
+++ server/main.js
@@ -1,2 +1,40 @@
+import { MeteorX } from 'meteor/meteorhacks:meteorx';
+
 import '../common/collections';
 import '../common/methods';
+
+if (process.env.USE_WORKAROUND) {
+  MeteorX.onReady(() => {
+    const proto = MeteorX.MongoOplogDriver.prototype;
+    const origHandleOplogEntryQuerying = proto._handleOplogEntryQuerying;
+
+    proto._handleOplogEntryQuerying = function wrappedHandleOplogEntryQuerying(op) {
+      if (op.o && op.o.$v) {
+        console.log("REMOVING $V from op", op);
+        delete op.o.$v;
+      }
+
+      if (op.o2 && op.o2.$v) {
+        console.log("REMOVING $V from op2", op);
+        delete op.o2.$v;
+      }
+
+      origHandleOplogEntryQuerying.call(this, op);
+    };
+
+    const origHandleOplogEntrySteadyOrFetching = proto._handleOplogEntrySteadyOrFetching;
+    proto._handleOplogEntrySteadyOrFetching = function wrappedHandleOplogEntrySteadyOrFetching(op) {
+      if (op.o && op.o.$v) {
+        console.log("REMOVING $V from op", op);
+        delete op.o.$v;
+      }
+
+      if (op.o2 && op.o2.$v) {
+        console.log("REMOVING $V from op2", op);
+        delete op.o2.$v;
+      }
+
+      origHandleOplogEntrySteadyOrFetching.call(this, op);
+    };
+  });
+}
